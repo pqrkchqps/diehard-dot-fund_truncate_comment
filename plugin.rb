cb98ebd5973f85067fe1d2f95caec9ddf7780ef6
pqrkchqps <johnacreps@musaemachine.com>
2017-07-14 09:58:46 -0500

    changed name to match repo

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,7 @@
 module Plugins
   module Diehard_FundTruncateComment
     class Plugin < Plugins::Base
-      setup! :diehard_fund_truncate_comment do |plugin|
+      setup! 'diehard-dot-fund_truncate_comment' do |plugin|
         plugin.enabled = true
         plugin.use_component :truncate_comment, outlet: :after_comment_body
         plugin.use_translations 'config/locales', :truncate_comment
